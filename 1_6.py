d67cf78de81f7ae0bda526d4d9c8bfcb1d2e610b
ElenaSt1978 <olololo50fomina@gmail.com>
2024-12-10 16:45:40 +0300

    1

---
+++ 1_6.py
@@ -0,0 +1,13 @@
+age={'Anna':41, 'Pavel':45, 'Dmitry':49, 'Helen':45}
+print(age)
+print(age['Dmitry'])
+print(age.get('Lidia'))
+age.update({'Maria':12, 'Kate':7})
+print(age)
+a=age.pop('Maria')
+print(age)
+set_={1, 'Anna', 'Pavel', 2 ,'Anna', 2 , True}
+print(set_)
+set_.update({'Tom', 7})
+set_.remove('Pavel')
+print(set_)
